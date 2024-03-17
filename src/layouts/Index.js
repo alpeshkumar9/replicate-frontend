1db682a980d0ef2c1767b45316bf3c6d5984d06f
Alpesh Kumar <alpesh2010kumar@gmail.com>
2024-03-17 18:06:32 +0000

    add MainNavbar

--- src/layouts/Index.js
+++ src/layouts/Index.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useLocation, Route, Routes, Navigate } from "react-router-dom";
+import MainNavbar from "../components/Navbars/MainNavbar";
 import routes from "../routes";
 
-const IndexLayout = () => {
+const IndexLayout = (props) => {
   const mainContent = React.useRef(null);
   const location = useLocation();
 
@@ -23,9 +24,26 @@ const IndexLayout = () => {
       }
     });
   };
+
+  const getBrandText = (path) => {
+    for (let i = 0; i < routes.length; i++) {
+      if (
+        props?.location?.pathname.indexOf(routes[i].layout + routes[i].path) !==
+        -1
+      ) {
+        return routes[i].name;
+      }
+    }
+    return "Brand";
+  };
+
   return (
     <>
       <div className="main-content" ref={mainContent}>
+        <MainNavbar
+          {...props}
+          brandText={getBrandText(props?.location?.pathname)}
+        />
         <Routes>
           {getRoutes(routes)}
         </Routes>
