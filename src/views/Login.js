6fa06bfcff86991c2c287ca913e4533571ce5441
Alpesh Kumar <alpesh2010kumar@gmail.com>
2024-03-17 23:46:37 +0000

    fixed error handling

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,29 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-
-import {
-    Button,
-    Card,
-    CardHeader,
-    CardBody,
-    FormGroup,
-    Form,
-    Input,
-    InputGroup,
-    Row,
-    Col
-} from "reactstrap";
+import { Button, Card, CardHeader, CardBody, FormGroup, Form, Input, InputGroup, Row, Col } from "reactstrap";
 import Header from "../components/Headers/Header";
 import { login } from "../network/ApiAxios";
 
-const Login = props => {
-
-    const [username, setEmail] = useState("");
+const Login = () => {
+    const [username, setUsername] = useState(""); // Corrected from setEmail to setUsername
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const navigate = useNavigate();
 
     const tryLogin = async () => {
+        // Check if username or password is empty and set an error message if so
+        if (!username.trim() || !password.trim()) {
+            setError("Username and password are required.");
+            return;
+        }
+
         const data = await login(username, password);
         if (data.success) {
             setError("");
@@ -33,44 +26,44 @@ const Login = props => {
             navigate('/');
         } else {
             setPassword("");
-            setError(data.msg);
+            setError(data.non_field_errors);
+            return;
         }
-    }
+    };
 
     return (
         <>
             <Header />
             <div className="mt--8 pb-5 container">
-                <div className="justify-content-center row">
+                <Row className="justify-content-center">
                     <Col lg="5" md="7">
                         <Card className="bg-secondary shadow border-0">
                             <CardHeader className="bg-transparent">
                                 <div className="text-muted text-center mt-2">
                                     <small>Sign in with credentials</small>
                                 </div>
-
                             </CardHeader>
                             <CardBody className="px-lg-5 py-lg-5">
                                 <Form role="form">
                                     <FormGroup className="mb-3">
                                         <InputGroup className="input-group-alternative">
                                             <Input placeholder="Username" type="username" autoComplete="username" value={username}
-                                                onChange={e => setEmail(e.target.value)} />
+                                                onChange={e => setUsername(e.target.value)} required />
                                         </InputGroup>
                                     </FormGroup>
                                     <FormGroup>
                                         <InputGroup className="input-group-alternative">
-                                            <Input placeholder="Password" type="password" autoComplete="password" value={password}
-                                                onChange={e => setPassword(e.target.value)} />
+                                            <Input placeholder="Password" type="password" autoComplete="current-password" value={password}
+                                                onChange={e => setPassword(e.target.value)} required />
                                         </InputGroup>
                                     </FormGroup>
-                                    {error ?
+                                    {error && (
                                         <div className="text-muted font-italic">
                                             <small>
-                                                error:{" "}
                                                 <span className="text-red font-weight-700">{error}</span>
                                             </small>
-                                        </div> : null}
+                                        </div>
+                                    )}
                                     <div className="text-center">
                                         <Button className="my-4" color="primary" type="button" onClick={tryLogin}>
                                             Sign in
@@ -79,12 +72,11 @@ const Login = props => {
                                 </Form>
                             </CardBody>
                         </Card>
-
                     </Col>
-                </div>
-            </div >
+                </Row>
+            </div>
         </>
     );
-}
+};
 
 export default Login;
