1d12e2a8c8917a371c6fb0b20aaf0a665ff94b5e
Alpesh Kumar <alpesh2010kumar@gmail.com>
2024-03-19 22:45:36 +0000

    fix prod config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    API_BASE_URL: 'http://localhost:8000/api/',
-    DOMAIN_NAME: 'http://localhost:3000',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+    DOMAIN_NAME: process.env.REACT_APP_DOMAIN_NAME,
 }
 
 export default config;
